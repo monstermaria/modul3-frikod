3c984200181c3a169315651775ebd83283efe8fa
monstermaria <42701428+monstermaria@users.noreply.github.com>
2020-09-11 21:59:39 +0200

    Added examples of promises and async/await

--- app.js
+++ app.js
@@ -30,3 +30,67 @@ clipboardButton.addEventListener("click", (event) => {
         }
     );
 });
+
+function getAnimalPromise(animal) {
+    const animals = [
+        "cat",
+        "dog",
+        "horse",
+        "snake",
+        "frog",
+        "rabbit",
+        "rat",
+        "lion",
+        "tiger"
+    ];
+
+    if (animals.includes(animal)) {
+        return Promise.resolve("I have heard of the animal " + animal);
+    } else {
+        return Promise.reject("I do not know of this animal " + animal);
+    }
+}
+
+document.querySelector("#animal-input").addEventListener("keyup", (event) => {
+    if (event.keyCode === 13) {
+        let result = "";
+        let color = "";
+
+        getAnimalPromise(event.target.value)
+            .then((text) => {
+                result = text;
+                color = "green";
+            })
+            .catch((text) => {
+                result = text;
+                color = "red";
+            })
+            .finally(() => {
+                const animalResult = document.querySelector("#animal-result");
+                animalResult.innerText = result;
+                animalResult.style.color = color;
+            });
+
+        event.target.value = "";
+    }
+});
+
+async function getCatPicture() {
+    const kitties = await fetch("https://api.thecatapi.com/v1/images/search");
+    if (kitties.status !== 200) {
+        document.querySelector("#error-message").innerText =
+            "Cat-astrofy has struck! No kitties delivered because: " +
+            kitties.status;
+    } else {
+        const kittiesJSON = await kitties.json();
+        const kittyUrl = kittiesJSON[0].url;
+        document.querySelector("#kitty-image").src = kittyUrl;
+        document.querySelector("#error-message").innerText = "";
+    }
+}
+
+document
+    .querySelector("#kitty-button")
+    .addEventListener("click", getCatPicture);
+
+getCatPicture();
